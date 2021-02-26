76491d9e798da2ff2272f59609b657f394ab0a7f
Aurélien <aureliendu3809@outlook.fr>
2021-02-26 19:25:34 +0100

    problem

--- view/view_profile.php
+++ view/view_profile.php
@@ -6,7 +6,6 @@
             <div class="picture">
                 <img class="pictureProfile" src="./public/image/profile/DE-CILLIA_Aurelien.jpg" alt="">
             </div>
-
             <div class="text">
                 <div class="underPicture">
                     <h1>DE CILLIA Aurélien</h1>
@@ -33,9 +32,36 @@
                 </div>
                 
             </div>
+        </div>
 
-            
-
+        <div class="blocInfo">
+            <div class="picture">
+                <img class="pictureProfile" src="./public/image/profile/Music.jpg" alt="">
+            </div>
+            <div class="text">
+                <div class="underPicture">
+                    <p>Je joue de la guitare électrique depuis 9 ans dans une école de musique.<br>
+                    Nous avons des groupes avec les quels nous organisons des concert dans des 
+                    restaurant comme le Ninkasi et autres.<br> 
+                    </p><br>
+                    <p>
+                        Je joue aussi du piano de la batterie, de la mendoline et autre encore...
+                    </p>
+                    
+                </div>
+                <div class="notUnderPicture">
+                    <p>Soft-Skills obtenu:</p>
+                    <ul class="listMargin">
+                        <li>
+                            Leader-Sheep
+                        </li>
+                        <li>
+                            Autodiscipline
+                        </li>
+                    </ul>
+                </div>
+                
+            </div>
         </div>
     </div>
     <div class="halfdiv">
